2291ffdc71f21daac45cdd5b563e3f76a556ce8b
GrieferPig <grieferpig@163.com>
2026-05-03 23:52:12 -0700

    feat: android build

---
+++ src-tauri/gen/android/app/src/main/java/com/grieferpig/jukeboy_companion/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.grieferpig.jukeboy_companion
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
